462d9a9e40110ecb688a87ac20def25ccbe8f3f5
Juliette Cordor <grassojames5@gmail.com>
2022-08-01 10:23:52 +1000

    updates

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,37 @@
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
-use syn::{parse_macro_input, AttributeArgs, ItemFn, NestedMeta};
+use quote::{quote, ToTokens};
+use syn::{parse_macro_input, AttributeArgs, ItemFn, Lit, NestedMeta};
 
 #[proc_macro_attribute]
 pub fn logging_gen(args: TokenStream, input: TokenStream) -> TokenStream {
+    let fn_decl = parse_macro_input!(input as ItemFn);
+
     let args = parse_macro_input!(args as AttributeArgs);
+    let fn_ident = &fn_decl.sig.ident;
+    let mut print_message = quote! { "The #fn_ident function was called" };
 
     for arg in args {
         match arg {
-            NestedMeta::Lit(x) => {}
+            NestedMeta::Lit(x) => match x {
+                Lit::Str(x) => {
+                    print_message = x.to_token_stream();
+                }
+                Lit::ByteStr(_) => todo!(),
+                Lit::Byte(_) => todo!(),
+                Lit::Char(_) => todo!(),
+                Lit::Int(_) => todo!(),
+                Lit::Float(_) => todo!(),
+                Lit::Bool(_) => todo!(),
+                Lit::Verbatim(_) => todo!(),
+            },
             _ => (),
         }
     }
 
-    let fn_decl = parse_macro_input!(input as ItemFn);
-
-    quote! {}.into()
+    quote! {
+        fn #fn_ident() {
+            println!(#print_message);
+        }
+    }
+    .into()
 }
--- tests/mod.rs
+++ tests/mod.rs
@@ -1,7 +1,7 @@
 use log_gen::logging_gen;
 
 #[test]
-#[logging_gen(yo = "message slay")]
+#[logging_gen]
 fn this_was_called() {
     let mut x = 5;
 }
