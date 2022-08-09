b9ccd9803c3aa0025dcfe0ceb6451fa70a91d973
Juliette Cordor <grassojames5@gmail.com>
2022-08-09 12:58:04 +1000

    fix

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,7 +1,24 @@
 use sourcegen::sourcegen;
 
+fn init_sub() {
+    use tracing::Level;
+    use tracing_subscriber::*;
+
+    // a builder for `FmtSubscriber`.
+    let subscriber = FmtSubscriber::builder()
+        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+        // will be written to stdout.
+        .with_max_level(Level::TRACE)
+        // completes the builder.
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+}
+
 #[test]
 fn test_with_args() {
+    init_sub();
+
     #[sourcegen("\"{fn_name}\" was called with variable \"arg1 = {arg1}\"")]
     fn with_args(arg1: &str) {}
 
@@ -9,11 +26,21 @@ fn test_with_args() {
 }
 
 #[test]
-#[sourcegen("Function was called without fn name as an arg")]
-fn test_no_fn_name() {}
+fn test_no_fn_name() {
+    init_sub();
+
+    #[sourcegen("Function was called without fn name as an arg")]
+    fn no_fn_name() {}
+
+    no_fn_name();
+}
 
 #[test]
 fn test_async() {
+    init_sub();
+
     #[sourcegen("\"async\" fn was called with variable \"arg1 = {arg1}\"")]
     async fn with_args(arg1: &str) {}
+
+    with_args("first argument");
 }
