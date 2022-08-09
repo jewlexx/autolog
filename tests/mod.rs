9d34ddf5e40bd389849cda6320411f6cc44c1713
Juliette Cordor <grassojames5@gmail.com>
2022-08-09 12:36:02 +1000

    fixed tests

--- tests/mod.rs
+++ tests/mod.rs
@@ -2,8 +2,8 @@ use log_gen::logging_gen;
 
 #[test]
 fn test_with_args() {
-    #[logging_gen("{fn_name} message go here with variable {arg1}")]
+    #[logging_gen("\"{fn_name}\" was called with variable \"arg1 = {arg1}\"")]
     fn with_args(arg1: &str) {}
 
-    with_args("arg1");
+    with_args("first argument");
 }
