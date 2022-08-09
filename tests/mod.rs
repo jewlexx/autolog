b5ec6366f50e985c9a5e500c593b2ecd73a798c5
Juliette Cordor <grassojames5@gmail.com>
2022-08-09 12:34:02 +1000

    fixed issues

--- tests/mod.rs
+++ tests/mod.rs
@@ -1,9 +1,9 @@
 use log_gen::logging_gen;
 
-#[logging_gen("{fn_name} message go here with variable {yo}")]
-fn this_was_called(yo: &str) {
-    let mut x = 5;
-}
+#[test]
+fn test_with_args() {
+    #[logging_gen("{fn_name} message go here with variable {arg1}")]
+    fn with_args(arg1: &str) {}
 
-#[logging_gen]
-async fn this_wasnt_called() {}
+    with_args("arg1");
+}
