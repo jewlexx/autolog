4c3dad681f1fcbc08eea471eb88167ffdca34df6
Juliette Cordor <grassojames5@gmail.com>
2022-09-09 15:36:38 +1000

    fixed issues with tests

--- tests/tracing.rs
+++ tests/tracing.rs
@@ -4,22 +4,23 @@ use sourcegen::sourcegen;
 static TRACING_ENABLED: Mutex<bool> = Mutex::new(false);
 
 fn init_sub() {
-    if *TRACING_ENABLED.lock() {
-        return;
+    if !*TRACING_ENABLED.lock() {
+        *TRACING_ENABLED.lock() = true;
+
+        use tracing::Level;
+        use tracing_subscriber::*;
+
+        // a builder for `FmtSubscriber`.
+        let subscriber = FmtSubscriber::builder()
+            // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
+            // will be written to stdout.
+            .with_max_level(Level::TRACE)
+            // completes the builder.
+            .finish();
+
+        tracing::subscriber::set_global_default(subscriber)
+            .expect("setting default subscriber failed");
     }
-
-    use tracing::Level;
-    use tracing_subscriber::*;
-
-    // a builder for `FmtSubscriber`.
-    let subscriber = FmtSubscriber::builder()
-        // all spans/events with a level higher than TRACE (e.g, debug, info, warn, etc.)
-        // will be written to stdout.
-        .with_max_level(Level::TRACE)
-        // completes the builder.
-        .finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
 }
 
 #[test]
@@ -34,6 +35,8 @@ fn test_with_args() {
 
 #[test]
 fn test_no_fn_name() {
+    init_sub();
+
     #[sourcegen("Function was called without fn name as an arg")]
     fn no_fn_name() {}
 
@@ -42,6 +45,8 @@ fn test_no_fn_name() {
 
 #[test]
 fn test_async() {
+    init_sub();
+
     #[sourcegen("\"async\" fn was called with variable \"arg1 = {arg1}\"")]
     async fn with_args(arg1: &str) {}
 
